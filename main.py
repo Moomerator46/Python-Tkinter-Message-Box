2aed38cd3435cacb812c40f998e5ca5a199cd40e
Moomerator46 <97137823+Moomerator46@users.noreply.github.com>
2024-09-19 17:03:51 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import sys
+
+def show_popup():
+  messagebox.showinfo("title here", "message here") # adds an info message box
+  messagebox.showwarning("title here", "message here") # adds a warning message box
+  messagebox.showerror("title here", "message here") # adds an error message box
+  question = messagebox.askquestion("title here", "message here") # asks a yes/no question
+  if question == 'yes':
+    # if the question answer is yes
+    print("clicked yes')
+  else:
+    # if the question answer was no
+    print("clicked no")
+
+def close():
+  sys.exit()
+
+window = tk.Tk()
+window.title("title of the window here")
+
+txt = ttk.Label(window, text="text of label here")
+txt.pack(pady=65)
+btn = ttk.Button(master=window, text="Show Pop-Up", command=show_popup)
+btn.pack()
+ext_btn = ttk.Button(master=window, text="Close", command=close)
+ext_btn.pack()
+
+window.mainloop()
